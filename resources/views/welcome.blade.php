b2d6bdede63ff8b1ec84715ddafc2f9063160d07
DIMASARYAPRAYOGA. <email@anda.com>
2024-04-30 22:34:15 +0700

    soal nomor 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,7 @@
                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Menu
                 </button>
+                {{-- SOAL NOMOR 2 --}}
                 <ul class="dropdown-menu d-blok" style="width: 300px; right:0%; left:auto">
                     <li><a class="dropdown-item" href="#">Home</a></li>
                     <li><a class="dropdown-item" href="/nama">Produk</a></li>
